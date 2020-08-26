4d981c1c75868b7252f2fccdf0b232d9e6db654e
Igor <igorbojk@gmail.com>
2020-08-26 10:48:06 +0300

    add userIp

--- public/index.js
+++ public/index.js
@@ -1,10 +1,16 @@
 var time = 0;
+let userIp = '';
 
 window.onload = () => {
     const startTime = Date.now();
     const myInterval = setInterval( () => {
         time = Date.now() - parseInt(startTime);
     }, 1000);
+    fetch('https://api.ipify.org/?format=json')
+        .then(result => result.json())
+        .then(data => {
+            userIp = data.ip;
+        })
 }
 
 window.onbeforeunload = () => {
@@ -14,7 +20,8 @@ window.onbeforeunload = () => {
 window.onunload = () => {
     const data = {
         location: location.href,
-        timeOnPage: `${time / 1000}s`
+        timeOnPage: `${time / 1000}s`,
+        userIp
     }
     navigator.sendBeacon("/send", JSON.stringify(data));
 }
